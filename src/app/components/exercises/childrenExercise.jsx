be7e2709f0829a979fe4d8a048206777457876ca
medkov77 <medkov77@mail.rum>
2022-05-21 09:43:07 +0300

    children

--- src/app/components/exercises/childrenExercise.jsx
+++ src/app/components/exercises/childrenExercise.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 import CollapseWrapper from "../common/collapse";
+const NuberedComponents = ({ children }) => {
+    return React.Children.map(children, (child, index) => {
+        return (
+            <div className="d-flex">
+                {`${index + 1}.`} {child}
+            </div>
+        );
+    });
+};
 const ChildrenExercise = () => {
     return (
         <CollapseWrapper title="Упражнение">
@@ -10,10 +19,11 @@ const ChildrenExercise = () => {
                 <code>React.Children.map</code> так и{" "}
                 <code>React.Children.toArray</code>
             </p>
-
-            <Component />
-            <Component />
-            <Component />
+            <NuberedComponents>
+                <Component />
+                <Component />
+                <Component />
+            </NuberedComponents>
         </CollapseWrapper>
     );
 };
